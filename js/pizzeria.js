3e498bc9236bc0737904f4893f81d7b6905aee58
Midori Mori <zaseaweed@gmail.com>
2013-11-24 22:15:35 -0800

    no message

--- js/pizzeria.js
+++ js/pizzeria.js
@@ -72,17 +72,18 @@ $(function() {
 	    items: [] //stores all items
 	}; //Cart object
 
-	var itemIndex; //Total index number
+	var itemIndex = 0; //Total index number
 
 	//Adding item to cart
 	//Constructs new single-in-cart item object, to be added to cart object.
 	$('.add-to-cart').click(function(){
+		itemIndex++;
 	    var newCartItem = {
 	        type: this.getAttribute('data-type'),
 	        name: this.getAttribute('data-name'),
 	        size: this.getAttribute('data-size'),
 	        price: this.getAttribute('data-price'),
-	        index: itemIndex++;
+	        index: itemIndex;
 	    };
 
 	    this.attr('data-index', itemIndex);
